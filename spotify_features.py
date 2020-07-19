ac6440f6942700ad0fabf780a26528fc054f874c
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-19 22:54:17 +0200

    Added spotify features script

---
+++ spotify_features.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    TRACKS_PER_REQUEST = 50  # Spotify yields up to 50 audio features per request
+
+    df = pd.read_csv("spotify_billboard.csv")
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
+    track_ids = df["id"]
+    chunks = [
+        track_ids[x : x + TRACKS_PER_REQUEST]
+        for x in range(0, len(track_ids), TRACKS_PER_REQUEST)
+    ]
+    spotify_features_df = pd.DataFrame()
+
+    try:
+        for track_ids in tqdm(chunks):
+            features = sp.audio_features(track_ids)
+
+            for tid, feature in zip(track_ids, features):
+                del feature["type"]
+                d = df[df["id"] == tid].to_dict("r")[0]
+                d = {**d, **feature}
+                spotify_features_df = spotify_features_df.append(d, ignore_index=True)
+    except Exception as e:
+        print(e)
+        pass
+
+    spotify_features_df.to_csv("spotify_billboard_features.csv", index=False)
--- spotify_matcher.py
+++ spotify_matcher.py
@@ -5,6 +5,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from tqdm import tqdm
 
+
 def remove_bracketed_text(text):
     return re.sub("[\(\[].*?[\)\]]", "", text)
 
@@ -13,6 +14,9 @@ if __name__ == "__main__":
 
     # read charts / songs file
     df = pd.read_csv("billboard.csv")
+    df = df.drop(
+        [c for c in df.columns if "0.1" in c], axis=1
+    )  # remove some Unnamed cols
     df["SongID"] = df["title"] + df["artist"]
 
     # remove duplicates
@@ -21,40 +25,56 @@ if __name__ == "__main__":
     # create empty dataframes for Spotify / unmatched songs
     spotify_df = pd.DataFrame(columns=[])
     unmatched_df = pd.DataFrame(columns=[])
-    for idx, row in tqdm(df.iterrows(), total=len(df)):
-        matched = False
 
-        # search for a track using the track title.
-        # subsequently compare the artist(s) of the track and its title using Levenshtein distance
-        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-        results = sp.search(row.title, type="track")
-        for r in results["tracks"]["items"]:
-            artists = r["artists"]
-            popularity = r["popularity"]
-            name = r["name"]
-            artist_names = ", ".join([a["name"] for a in artists])
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+    try:
+        for idx, row in tqdm(df.iterrows(), total=len(df)):
+            matched = False
 
-            title_ratio = fuzz.ratio(remove_bracketed_text(row.title.lower()), remove_bracketed_text(name.lower()))
-            artist_ratio = fuzz.ratio(remove_bracketed_text(row.artist.lower().replace("featuring", "")), remove_bracketed_text(artist_names.lower()))
+            # search for a track using the track title.
+            # subsequently compare the artist(s) of the track and its title using Levenshtein distance
+            results = sp.search(row.title, type="track")
+            for r in results["tracks"]["items"]:
+                artists = r["artists"]
+                popularity = r["popularity"]
+                name = r["name"]
+                artist_names = ", ".join([a["name"] for a in artists])
 
-            if title_ratio > 90 and artist_ratio > 80:
-                d = {
-                    "title_ratio": title_ratio,
-                    "artist_ratio": artist_ratio,
-                    "popularity": popularity,
-                    "name": name,
-                    "artists": artist_names,
-                    "id": r["id"],
-                    "uri": r["uri"],
-                    "album": r["album"]["name"],
-                    "preview_url": r["preview_url"]
-                }
-                spotify_df = spotify_df.append(d, ignore_index=True)
-                matched = True
-                break
-        
-        if not matched:
-            unmatched_df = unmatched_df.append(row)
+                title_levenshtein = fuzz.ratio(
+                    remove_bracketed_text(row.title.lower()),
+                    remove_bracketed_text(name.lower()),
+                )
+                artist_levenshtein = fuzz.ratio(
+                    remove_bracketed_text(row.artist.lower().replace("featuring", "")),
+                    remove_bracketed_text(artist_names.lower()),
+                )
+
+                if title_levenshtein > 90 and artist_levenshtein > 80:
+                    d = {
+                        "title_levenshtein": title_levenshtein,
+                        "artist_levenshtein": artist_levenshtein,
+                        "popularity": popularity,
+                        "name": name,
+                        "artists": artist_names,
+                        "id": r["id"],
+                        "uri": r["uri"],
+                        "album": r["album"]["name"],
+                        "preview_url": r["preview_url"],
+                    }
+
+                    row_dict = dict(row)
+                    row_dict = {"billboard_" + k: v for k, v in dict(row).items()}
+                    d = {**d, **row_dict}
+                    spotify_df = spotify_df.append(d, ignore_index=True)
+                    matched = True
+                    break
+
+            if not matched:
+                unmatched_df = unmatched_df.append(row)
+
+    except Exception as e:
+        print(e)
+        pass
 
     print("Unmatched tracks:", len(unmatched_df))
     spotify_df.to_csv("spotify_billboard.csv")
